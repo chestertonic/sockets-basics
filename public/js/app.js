5f6ffa42c7d72abff58235498d9b23956ff7e33c
Chester <chestertonic@gmail.com>
2016-02-01 12:35:38 -0500

    Added message submit form

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,10 @@
 var socket = io();
+// cached elements
+var $form = $('#message-form');
+var $message = $('input[name=message]');
+var $messages = $('.messages');
+
+// socket events
 
 socket.on('connect', function() {
   console.log('Connected to socket.io server!');
@@ -7,4 +13,19 @@ socket.on('connect', function() {
 socket.on('message', function(message) {
   console.log('New Message: ');
   console.log(message.text);
+
+  $messages.append('<p>' + message.text + '</p>');
+});
+
+// message events
+
+$form.on('submit', function(event) {
+  event.preventDefault();
+
+  socket.emit('message', {
+    text: $message.val()
+  });
+
+  $message.val('');
+
 });
